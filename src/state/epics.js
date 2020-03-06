82b9e257b1d44ad3eec035d0b09880fddb824e91
ibarretorey <ibarretorey@gmail.com>
2020-03-06 14:35:44 -0300

    add logsEpics to combine epics

--- src/state/epics.js
+++ src/state/epics.js
@@ -1,5 +1,4 @@
 import { combineEpics } from 'redux-observable';
-
 import { epics as switchesEpics } from './switches';
 import { epics as nicsEpics } from './nics';
 import { epics as uiEpics } from './ui';
