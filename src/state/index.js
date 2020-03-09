dc0cb6b32e4f06469d083a9362493560a7172c0c
ibarretorey <ibarretorey@gmail.com>
2020-03-09 14:30:05 -0300

    se elimina observables de todos los componentes, previo cambio a redux thunk

--- src/state/index.js
+++ src/state/index.js
@@ -3,23 +3,20 @@ import {
   applyMiddleware,
   compose
 } from 'redux';
-import { createEpicMiddleware } from 'redux-observable';
+
 
 import rootReducer from './reducers.js';
-import rootEpic from './epics.js';
+
 import { loggerMiddleware } from './middleware';
 import { monitorEnhancer } from './enhancers';
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export function configureStore() {
-  const epicMiddleware = createEpicMiddleware();
-  const middlewareEnhancer = applyMiddleware(epicMiddleware, loggerMiddleware);
+  const middlewareEnhancer = applyMiddleware(loggerMiddleware);
   const composedEnhancers = composeEnhancers(middlewareEnhancer, monitorEnhancer);
 
   const store = createStore(rootReducer, undefined, composedEnhancers);
-  
-  epicMiddleware.run(rootEpic);
 
   return store;
 }
