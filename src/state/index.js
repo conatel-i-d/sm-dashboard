a112e1d6257e0ca189b3ccf2ba6d2a379ddf21af
ibarretorey <ibarretorey@gmail.com>
2020-03-20 11:50:47 -0300

    quedo un parentesis de menos

--- src/state/index.js
+++ src/state/index.js
@@ -6,12 +6,8 @@ import thunk from 'redux-thunk';
 
 const composeEnhancers =
   (typeof window !== 'undefined' &&
-<<<<<<< HEAD
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-=======
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
->>>>>>> c3e503faf1d09de8328d54682c404544a3ba48af
-  compose;
+  compose);
 
 export function configureStore() {
   const middlewareEnhancer = applyMiddleware(thunk);
